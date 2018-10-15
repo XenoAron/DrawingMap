7ba7638e3a74c5222dedd28960a745c0c1afea74
= <jwt1029@midasit.com>
2018-10-15 16:15:45 +0900

    init

---
+++ src/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * @메쏘드명 : MainClass
+ * @작성자 : jwt1029
+ * @작성일자 : 2018-10-15
+ * @설명 :
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        Long l = 0l;
+        System.out.println("Result : " + factorial(15));
+        for(int i=0;i<factorial(15);i++) {
+            l += 1;
+        }
+        System.out.println("Long : " + l);
+    }
+
+    private static int factorial(int n) {
+        return n == 1 ? 1 : n * factorial(n - 1);
+    }
+}
